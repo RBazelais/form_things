f71b28a23ec19ce19400bfa49418f0663017e436
rbazelais <bazelais.rachel@gmail.com>
2018-02-01 10:21:17 -0800

    Login stuff

--- apps/login/views.py
+++ apps/login/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render, HttpResponse, redirect
-
-
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .models import User
+# Create your views here.
 def index(request):
-    response = "Hello, I am your first request!"
-    return HttpResponse(response)
+    return render(request, "login/index.html", {'status':True})
+
+def processReg(request):
+    confirm = request.POST['confirm']
+    response = User.objects.validate(request.POST)
+    if not response['status']:
+        return render(request, "login/index.html", response)
+    request.session['user_id'] = response['user'].id
+    return redirect(reverse('dashboard:landing')) # named routing
+
+def processLog(request):
+    request.session['user_id'] = User.objects.get(email=request.POST['email']).id
+    return redirect('/dashboard/')
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include # Notice we added include
 from django.contrib import admin
 urlpatterns = [
     url(r'^', include('apps.login.urls')),
+    # url(r'^dashboard/', include('apps.dashboard.urls')),
     url(r'^admin/', admin.site.urls)
 ]
